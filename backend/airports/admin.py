f3b68484f44f3268ecbed77902973b09fb1dd22f
wtf403 <wtf403@yandex.ru>
2023-11-30 13:47:49 +0300

    working on schedules import

--- backend/airports/admin.py
+++ backend/airports/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from airports.models import Airport
 
-# Register your models here.
+
+class AirportAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [f.name for f in self.model._meta.fields]
+
+
+admin.site.register(Airport, AirportAdmin)
