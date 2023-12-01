e1214998f1a0bc38a452bc6f70b62ed0aa586f7d
VictorGod <savenckoa@gmail.com>
2023-12-01 09:50:12 +0300

    amenities-report

--- backend/amenities/views.py
+++ backend/amenities/views.py
@@ -17,80 +17,59 @@ class AmenityViewSet(viewsets.ModelViewSet):
     queryset = Amenity.objects.all()
     serializer_class = AmenitySerializer
 
-    @action(detail=False, methods=['post'], url_path="amenities-statistics")
-    def amenities_statistics(self, request):
+    @action(detail=False, methods=['post'], url_path="amenities-report")
+    def amenities_report(self, request):
         booking_reference = request.data.get('booking_reference')
-        if not booking_reference:
-            return Response({"error": "Booking reference is required."}, status=400)
-
-        try:
-            ticket = Ticket.objects.get(booking_reference=booking_reference)
-        except Ticket.DoesNotExist:
-            return Response({"error": "Ticket not found."}, status=404)
-
-        amenities_data = AmenityTicket.objects.filter(ticket=ticket).values(
-            'amenity__service', 'ticket__first_name', 'ticket__last_name', 'ticket__passport_number'
-        )
-
-        return Response({"class": ticket.cabin_type.name, "data": list(amenities_data)})
-    
-
-    @action(detail=False, methods=['post'], url_path="amenity-report")
-    def amenity_report(self, request):
         flight_id = request.data.get('flight_id')
-        departure_airport = request.data.get('departure_airport')
-        arrival_airport = request.data.get('arrival_airport')
+        start_time = request.data.get('start_time')
+        end_time = request.data.get('end_time')
 
-        if not flight_id or not departure_airport or not arrival_airport:
-            return Response({"error": "Flight ID, departure airport, and arrival airport are required."}, status=400)
+        if not booking_reference and not (flight_id and start_time and end_time):
+            return Response({"error": "Необходимо указать либо код бронирования, либо идентификатор рейса, начальное и конечное время."}, status=400)
 
         try:
-            schedule = Schedule.objects.get(pk=flight_id)
-            tickets = Ticket.objects.filter(schedule=schedule)
-            tickets = tickets.filter(
-                schedule__Route__DepartureAirport=departure_airport,
-                schedule__Route__ArrivalAirport=arrival_airport
-            )
+            if booking_reference:
+                ticket = Ticket.objects.get(booking_reference=booking_reference)
+                amenities_data = AmenityTicket.objects.filter(ticket=ticket).values(
+                    'amenity__name', 'ticket__cabin_type', 'ticket__first_name', 'ticket__last_name', 'ticket__passport_number'
+                )
+                return Response({"class": ticket.cabin_type.name, "data": list(amenities_data)})
+            
+            elif flight_id and start_time and end_time:
+                schedule = Schedule.objects.get(pk=flight_id)
+                tickets = Ticket.objects.filter(schedule=schedule, schedule__Date__gte=start_time, schedule__Date__lte=end_time)
 
-            amenities_data = AmenityTicket.objects.filter(ticket__in=tickets).values(
-                'amenity__service', 'ticket__cabin_type'
-            )
+                amenities_data = AmenityTicket.objects.filter(ticket__in=tickets).values(
+                    'amenity__name', 'ticket__cabin_type', 'ticket__first_name', 'ticket__last_name', 'ticket__passport_number'
+                )
 
-            # Format data as specified
-            amenities = [item['amenity__service'] for item in amenities_data]
-            statistics = {}
-            for item in amenities_data:
-                if item['ticket__cabin_type'] not in statistics:
-                    statistics[item['ticket__cabin_type']] = [0, 0, 0, 0]
+                # Форматирование данных как указано
+                amenities = [item['amenity__name'] for item in amenities_data]
+                statistics = {}
+                for item in amenities_data:
+                    if item['ticket__cabin_type'] not in statistics:
+                        statistics[item['ticket__cabin_type']] = [0, 0, 0, 0]
 
-                if item['amenity__service'] == 1:
-                    statistics[item['ticket__cabin_type']][0] += 1
-                elif item['amenity__service'] == 2:
-                    statistics[item['ticket__cabin_type']][1] += 1
-                elif item['amenity__service'] == 3:
-                    statistics[item['ticket__cabin_type']][2] += 1
-                elif item['amenity__service'] == 4:
-                    statistics[item['ticket__cabin_type']][3] += 1
+                    # Пример подсчета количества услуг для каждого класса
+                    if item['amenity__name'] == "Some Amenity":
+                        statistics[item['ticket__cabin_type']][0] += 1
+                    elif item['amenity__name'] == "Another Amenity":
+                        statistics[item['ticket__cabin_type']][1] += 1
 
-            result = {
-                "amenities": amenities,
-                "statistics": [
-                    {"class": key, "data": value} for key, value in statistics.items()
-                ]
-            }
 
-            return Response(result)
+                result = {
+                    "amenities": amenities,
+                    "statistics": [
+                        {"class": key, "data": value} for key, value in statistics.items()
+                    ]
+                }
 
-        except Schedule.DoesNotExist:
-            return Response({"error": "Flight not found."}, status=404)
+                return Response(result)
+
+        except (Ticket.DoesNotExist, Schedule.DoesNotExist):
+            return Response({"error": "Билет или рейс не найден."}, status=404)
         except Exception as e:
-            return Response({"error": f"An error occurred: {str(e)}"}, status=500)
-
-    @action(detail=False, url_path="amenities-cabin-type")
-    def amenities_cabin_type(self, request):
-        items = AmenityCabinType.objects.all()
-        serializer = AmenityCabinTypeSerializer(items, many=True)
-        return Response(serializer.data)
+            return Response({"error": f"Произошла ошибка: {str(e)}"}, status=500)
 
     @action(detail=False, url_path="amenities-tickets")
     def amenities_tickets(self, request):
