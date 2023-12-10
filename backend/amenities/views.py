3bfb1235a11993f711c2328a57ca3d7e6ca24287
VictorGod <savenckoa@gmail.com>
2023-12-10 22:18:25 +0300

    full-report

--- backend/amenities/views.py
+++ backend/amenities/views.py
@@ -14,7 +14,7 @@ from schedules.models import Schedule
 from office.models import Office
 from django.db.models import Count
 from authentication.models import User
-
+from datetime import datetime, timedelta
 
 class AmenityViewSet(viewsets.ModelViewSet):
     queryset = Amenity.objects.all()
@@ -112,19 +112,12 @@ class AmenityViewSet(viewsets.ModelViewSet):
             "ticket__passport_number",
         )
 
-        amenities_result = {
-            "amenities": list(amenities_data),
-        }
-
         # Получение топ клиентов
         top_clients = (
             Ticket.objects.values("user__email")
             .annotate(total_tickets=Count("id"))
             .order_by("-total_tickets")[:5]
         )
-        top_clients_result = {
-            "top_clients": list(top_clients),
-        }
 
         # Получение топ офисов
         top_offices = (
@@ -132,15 +125,43 @@ class AmenityViewSet(viewsets.ModelViewSet):
             .annotate(total_tickets=Count("user__id"))
             .order_by("-total_tickets")[:5]
         )
-        top_offices_result = {
-            "top_offices": list(top_offices),
-        }
+
+        # Получение количества confirmed и canceled рейсов
+        flight_status_counts = (
+            Ticket.objects.values("confirmed")
+            .annotate(count=Count("id"))
+            .order_by("-count")
+        )
+
+        confirmed_flights_count = 0
+        canceled_flights_count = 0
+        for status_count in flight_status_counts:
+            if status_count["confirmed"]:
+                confirmed_flights_count = status_count["count"]
+            else:
+                canceled_flights_count = status_count["count"]
+
+        # Calculate average flight duration
+        confirmed_tickets = Ticket.objects.filter(confirmed=True)
+        total_duration = timedelta()
+        for ticket in confirmed_tickets:
+            flight_start_time = datetime.combine(ticket.schedule.Date, ticket.schedule.Time)
+            flight_end_time = flight_start_time + timedelta(minutes=ticket.schedule.Route.FlightTime)
+            duration = flight_end_time - flight_start_time
+            total_duration += duration
+
+        average_duration = total_duration / len(confirmed_tickets)
 
         # Общий результат
         result = {
-            "amenities": amenities_result,
-            "top_clients": top_clients_result,
-            "top_offices": top_offices_result,
+            "amenities": {"amenities": list(amenities_data)},
+            "top_clients": {"top_clients": list(top_clients)},
+            "top_offices": {"top_offices": list(top_offices)},
+            "flight_counts": {
+                "confirmed_flights_count": confirmed_flights_count,
+                "canceled_flights_count": canceled_flights_count,
+            },
+            "average_flight_duration": str(average_duration),
         }
 
         return Response(result)
